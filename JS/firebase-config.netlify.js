1a2f0af50bf26e4a21973f1072ac15087989c889
moaaskt <moacirmayer@hotmail.com>
2025-10-26 22:21:52 -0300

    feat(firebase): add netlify-specific config loading and theme polish
    Add conditional loading of firebase-config.netlify.js for netlify deployments
    Update firebase-config.js to check for window.__PETFLIX_KEYS
    Apply Petflix theme variables and polish to style-filmes.css

---
+++ JS/firebase-config.netlify.js
@@ -0,0 +1,15 @@
+// Chaves de PRODUÇÃO (expostas no cliente; restrinja por HTTP referrer no GCP!)
+window.__PETFLIX_KEYS = Object.assign({}, window.__PETFLIX_KEYS, {
+  firebase: {
+    apiKey: "AIzaSyAb_0r54B3fL3CmLukhuXYgtxfIpj9IgvU",
+    authDomain: "petflix-de1c3.firebaseapp.com",
+    databaseURL: "https://petflix-de1c3-default-rtdb.firebaseio.com",
+    projectId: "petflix-de1c3",
+    storageBucket: "petflix-de1c3.appspot.com",
+    messagingSenderId: "863177295284",
+    appId: "1:863177295284:web:SEU_APPID"
+  },
+  youtube: {
+    apiKey: "AIzaSyAiqiAQ8rFDwxO4IwuRqO2mfTSsirA3fX4" // restrita a `https://flixpet.netlify.app/*`
+  }
+});
